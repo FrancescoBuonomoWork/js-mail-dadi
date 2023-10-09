dfb42d040d7500cc7308ae4aa506dc16723e70d7
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-09 16:27:41 +0200

    add DOM element

--- js/dadi.js
+++ js/dadi.js
@@ -1,13 +1,20 @@
+const appDOMElement = document.getElementById("app");
+const h1DOMElement = document.createElement("h1");
+
+
 const userNumber = Math.floor(Math.random() * (6 - 1) + 1);
 console.log(userNumber);
 
 const computerNumber = Math.floor(Math.random() * (6 - 1) + 1);
 console.log(computerNumber);
-
+let message;
 if (userNumber > computerNumber) {
     console.log("hai vinto");
+    message = "Hai vinto";
 } else {
     console.log("hai perso");
+    message = "Hai perso";
 }
-
+h1DOMElement.innerHTML = message;
+appDOMElement.append(h1DOMElement);
 
