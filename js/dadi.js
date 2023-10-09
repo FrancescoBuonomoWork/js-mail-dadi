c0721cd37fdb306f6db04bb773b463a2b75b9610
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-09 16:23:32 +0200

    add js elements

--- js/dadi.js
+++ js/dadi.js
@@ -1,1 +1,13 @@
-let randomNumber = Math.floor(Math.random() * (max - min) + min);
+const userNumber = Math.floor(Math.random() * (6 - 1) + 1);
+console.log(userNumber);
+
+const computerNumber = Math.floor(Math.random() * (6 - 1) + 1);
+console.log(computerNumber);
+
+if (userNumber > computerNumber) {
+    console.log("hai vinto");
+} else {
+    console.log("hai perso");
+}
+
+
