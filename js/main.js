a751ef31685967a6b1989c81629a7ebc50e91291
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-09 15:15:02 +0200

    add some element

--- js/main.js
+++ js/main.js
@@ -4,18 +4,23 @@ const arrayEmail = ["antonio@gmail.com", "maria@gmail.com","rosa@gmail.com", "lu
 console.log(arrayEmail);
 // chiedi all'utente la sua email (PROMPT)
 
-// const userEmail = prompt("Inserisci la tua email");
+const userEmail = prompt("Inserisci la tua email");
 // seleziona in modo separato ogni elementro che fa parte della lista
 for (let i = 0; i < arrayEmail.length; i++) {
 
     const currentEmail = arrayEmail[i];
     console.log(i,currentEmail);
+    if (userEmail === currentEmail) {
+        let message = "l'email è valida";
+        console.log(message);
+    } 
+    else {
+        message = "l'email non è valida";
+        console.log(message);
+    }
 }
 // verfica che l' email inserita dall'utente sia nella lista
 
 //     - stampa un messaggio appropriato per l'esito dell'operazione di verifica
 //         - SE l'email è presente nella lista(condizione vera) stampiamo un esito positivo
-// if (userEmail === arrayEmail[0]) {
-//     console.log("l'email è valida")
-// }
 //         ALTRIMENTI l'email non è nella lista stampiamo un esito negativo
